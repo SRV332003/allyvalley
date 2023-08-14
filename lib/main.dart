fba12101601377276f1516037be690db93d26069
thakurajay369 <ajthakur1205@gmail.com>
2023-08-14 19:19:53 +0530

    Pulling for making splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -10,31 +10,66 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: const TabBar(
-              tabs: [
-                Tab(
-                  icon: Icon(Icons.home_filled),
-                ),
-                Tab(
-                  icon: Icon(Icons.search_outlined),
-                ),
-                Tab(
-                  icon: Icon(Icons.settings),
-                )
-              ],
-            ),
-          ),
+      home: const TabManager(),
+    );
+  }
+}
+
+class TabManager extends StatefulWidget {
+  const TabManager({super.key});
+
+  @override
+  State<TabManager> createState() => _TabManagerState();
+}
+
+class _TabManagerState extends State<TabManager> {
+  var index = 0;
+  final list = const [
+    Center(child: Text("Hello")),
+    Center(child: Text("Hey")),
+    Center(child: Text("There")),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        centerTitle: true,
+        title: const Text(
+          "AllyValley",
+          style: TextStyle(color: Colors.white),
         ),
-      )
+      ),
+      body: list[index],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (value) {
+          setState(() {
+            index = value;
+          });
+        },
+        currentIndex: index,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "Search",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
+          ),
+        ],
+      ),
     );
   }
 }
